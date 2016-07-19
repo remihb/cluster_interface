53424350d4f1b8a7722738b10d0bc6ede665809a
Rémi Hauperpin-Bonneau <remi.hauperpinbonneau@live.fr>
2016-07-19 17:16:10 +0200

    generating yeoman angular project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clusterInterfaceApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the clusterInterfaceApp
+ */
+angular.module('clusterInterfaceApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clusterInterfaceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the clusterInterfaceApp
+ */
+angular.module('clusterInterfaceApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
