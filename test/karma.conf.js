7e447e5b36d5291fcd7e61ece1a736b8f49e5e45
Rémi Hauperpin-Bonneau <remi.hauperpinbonneau@live.fr>
2016-07-21 11:25:09 +0200

    add angular-ui-select & lodash

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -31,6 +31,10 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/select2/select2.js',
+      'bower_components/angular-ui-select3/src/select3.js',
+      'bower_components/angular-ui-select/dist/select.js',
+      'bower_components/lodash/lodash.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
