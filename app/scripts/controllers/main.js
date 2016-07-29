d90810037f3c30c8f83532ca705ee157e174b314
Rémi Hauperpin-Bonneau <remi.hauperpinbonneau@live.fr>
2016-07-29 11:11:00 +0200

    implements drag and drop modules feature

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,3 +1,10 @@
+/* jshint
+laxcomma:true
+, laxbreak:true
+, unused : false
+, loopfunc: true
+*/
+
 'use strict';
 
 /**
@@ -8,11 +15,33 @@
 * Controller of the clusterInterfaceApp
 */
 angular.module('clusterInterfaceApp')
-.controller('MainCtrl', function ($scope) {
-    $scope.moduleList = [{name:'salut'}, {name:'bla'}];
-    $scope.module = {};
+.controller('MainCtrl', function ($scope, $rootScope) {
+    $scope.moduleList = _.sortBy([
+        new Component_type("encoder", "Allow to encode multiple video file format"), new Component_type("encode-parser"), new Component_type("decoder"), new Component_type("Component")
+    ], function(mod){ return mod.getName().toLowerCase();});
 
     $scope.changePlaceholder = function(){
         $('.ui-select-search').attr('placeholder', 'Filter by name');
     };
+
+    $scope.getModule = function(module){
+        $scope.selectedModule = module;
+    };
+    $scope.flow = [];
+    $rootScope.$on('dropEvent', function (evt, dragged, dropped) {
+        var indexes;
+        if (dropped === "newline"){
+            $scope.flow.push([dragged.getName()]);
+        }
+        else if (dropped.indexOf("newcol") >= 0){
+            indexes = dropped.split('-');
+            $scope.flow[indexes[0]].push(dragged.getName());
+        }
+        else{
+            indexes = dropped.split('-');
+            $scope.flow[indexes[0]][indexes[1]] = dragged.getName();
+        }
+        $scope.$apply();
+    });
+
 });
