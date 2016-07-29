4ab119089a47b3b0da77f4dbed0e1e5957fde33d
Rémi Hauperpin-Bonneau <remi.hauperpinbonneau@live.fr>
2016-07-29 11:09:10 +0200

    modify to filter on whole world

--- app/scripts/filters/listfilter.js
+++ app/scripts/filters/listfilter.js
@@ -14,6 +14,8 @@ angular.module('clusterInterfaceApp')
         expression = (expression === undefined) ? "" : expression;
         return (array === undefined) ? null : _.filter(array, function(elem){
             return (elem.name.toLowerCase().indexOf(expression.toLowerCase()) === 0);
-        });
+        }).concat(_.filter(array, function(elem){
+            return (elem.name.toLowerCase().indexOf(expression.toLowerCase()) > 0);
+        }));
     };
 });
